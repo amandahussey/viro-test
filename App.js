c742a9d48fdda0acb3205639dde9ca14c9f9cfc7
Eddie Canales <eddiecanales@gmail.com>
2022-09-09 13:05:25 -0400

    add support for irises at four corners that let you place the spinner in the center of the "tray"

--- App.js
+++ App.js
@@ -22,13 +22,69 @@ const HelloWorldSceneAR = () => {
   const [text, setText] = useState('Initializing AR...');
 
   // register target
+  const IRIS_TARGET_METERS = 0.025;
+  const IRIS_TARGET_METERS_SMALL = IRIS_TARGET_METERS/2;
   ViroARTrackingTargets.createTargets({
-    iris: {
-      source: require('./res/iris.jpeg'),
+    irisGreenBg: {
+      source: require('./res/iris-green-bg.png'),
       orientation: "Up",
-      physicalWidth: 0.0508, // real world width in meters
+      physicalWidth: IRIS_TARGET_METERS, // real world width in meters
       type: 'Image',
-    }
+    },
+    irisGreen: {
+      source: require('./res/iris-green.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS, // real world width in meters
+      type: 'Image',
+    },
+    irisPurpleBg: {
+      source: require('./res/iris-purple-bg.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS, // real world width in meters
+      type: 'Image',
+    },
+    irisPurple: {
+      source: require('./res/iris-purple.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS, // real world width in meters
+      type: 'Image',
+    },
+    irisGrid: {
+      source: require('./res/iris-grid-colors.png'),
+      orientation: "Up",
+      physicalWidth: 0.02, // real world width in meters
+      type: 'Image',
+    },
+    colorGrid: {
+      source: require('./res/iris-grid-colors-only.png'),
+      orientation: "Up",
+      physicalWidth: 0.02, // real world width in meters
+      type: 'Image',
+    },
+    irisGreenBgSmall: {
+      source: require('./res/iris-green-bg.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS_SMALL, // real world width in meters
+      type: 'Image',
+    },
+    irisGreenSmall: {
+      source: require('./res/iris-green.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS_SMALL, // real world width in meters
+      type: 'Image',
+    },
+    irisPurpleBgSmall: {
+      source: require('./res/iris-purple-bg.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS_SMALL, // real world width in meters
+      type: 'Image',
+    },
+    irisPurpleSmall: {
+      source: require('./res/iris-purple.png'),
+      orientation: "Up",
+      physicalWidth: IRIS_TARGET_METERS_SMALL, // real world width in meters
+      type: 'Image',
+    },
   });
 
   ViroMaterials.createMaterials({
@@ -62,13 +118,13 @@ const HelloWorldSceneAR = () => {
 
       {/* <ViroSkyBox color={'blue'}></ViroSkyBox> */}
 
-      <ViroARImageMarker target="iris" onAnchorFound={(anchor) => console.log('onAnchorFound marker', anchor.trackingMethod)}>
+      <ViroARImageMarker target="irisPurple" onAnchorFound={(anchor) => console.log('onAnchorFound marker', anchor.trackingMethod)}>
         <ViroSpinner
             type='light'
             scale={[0.1, 0.1, 1]}
             rotation={[-90, 0, 0]}
             opacity={0.5}
-            position={[0, -0.01, 0]}
+            position={[-0.06, -0.01, -0.06]}
             materials={['spinner']}
         />
         {/* <ViroBox position={[0, 0, 0]}
@@ -80,7 +136,7 @@ const HelloWorldSceneAR = () => {
             width={2}
             height={2}
             scale={[0.02, 0.02, 0.02]}
-            position={[0, 0.05, 0]}
+            position={[-0.06, 0.05, -0.06]}
             style={styles.helloWorldTextStyle}
           />
         {/*<ViroNode position={[0,0,-1]}>
